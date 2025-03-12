7ad2e55db2d5ea6b5c2c9783c0cd981d783092db
samarth-sheth-us <samarth.sheth@logicwind.com>
2025-03-12 15:24:47 +0530

    change

--- ios/StatusBarHeightModule.mm
+++ ios/StatusBarHeightModule.mm
@@ -1,30 +1,10 @@
-#import <React/RCTUtils.h>
-#import <UIKit/UIKit.h>
 #import "StatusBarHeightModule.h"
+#import <React/RCTUtils.h>
 
 @implementation StatusBarHeightModule
 
 RCT_EXPORT_MODULE()
 
-- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
-    (const facebook::react::ObjCTurboModule::InitParams &)params
-{
-    return std::make_shared<facebook::react::NativeStatusBarHeightSpecJSI>(params.jsInvoker);
-}
-
-- (facebook::jsi::Value)getHeight:(facebook::jsi::Runtime &)rt
-{
-    float statusBarHeight;
-    if (@available(iOS 13.0, *)) {
-        UIWindowScene *windowScene = (UIWindowScene *)[UIApplication sharedApplication].connectedScenes.allObjects.firstObject;
-        statusBarHeight = windowScene.statusBarManager.statusBarFrame.size.height;
-    } else {
-        statusBarHeight = [UIApplication sharedApplication].statusBarFrame.size.height;
-    }
-    return facebook::jsi::Value(statusBarHeight);
-}
-
-// For backward compatibility with the old architecture
 RCT_EXPORT_METHOD(getHeight:(RCTPromiseResolveBlock)resolve
                   reject:(RCTPromiseRejectBlock)reject)
 {
@@ -38,4 +18,4 @@ RCT_EXPORT_METHOD(getHeight:(RCTPromiseResolveBlock)resolve
     resolve(@(statusBarHeight));
 }
 
-@end 
+@end
